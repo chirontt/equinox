f7a0c7b15776870a24ba1f625628ee9ba69a4e0a
Tue Ton <49886739+chirontt@users.noreply.github.com>
2024-08-31 13:51:40 -0400

    add FreeBSD platforms' build

--- bundles/org.eclipse.equinox.launcher/src/org/eclipse/equinox/launcher/Main.java
+++ bundles/org.eclipse.equinox.launcher/src/org/eclipse/equinox/launcher/Main.java
@@ -383,6 +383,8 @@ public class Main {
 			return Constants.WS_WIN32;
 		if (osName.equals(Constants.OS_LINUX))
 			return Constants.WS_GTK;
+		if (osName.equals(Constants.OS_FREEBSD))
+			return Constants.WS_GTK;
 		if (osName.equals(Constants.OS_MACOSX))
 			return Constants.WS_COCOA;
 		if (osName.equals(Constants.OS_HPUX))
@@ -412,6 +414,8 @@ public class Main {
 			return Constants.OS_SOLARIS;
 		if (osName.equalsIgnoreCase(Constants.INTERNAL_OS_LINUX))
 			return Constants.OS_LINUX;
+		if (osName.equalsIgnoreCase(Constants.INTERNAL_OS_FREEBSD))
+			return Constants.OS_FREEBSD;
 		if (osName.equalsIgnoreCase(Constants.INTERNAL_OS_QNX))
 			return Constants.OS_QNX;
 		if (osName.equalsIgnoreCase(Constants.INTERNAL_OS_AIX))
